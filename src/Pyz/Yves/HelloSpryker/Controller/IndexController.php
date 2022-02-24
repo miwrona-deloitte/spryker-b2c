ec19e8078ea68d461d1b5fec32b8477b2d63f882
Michal Wrona <miwrona@deloittece.com>
2022-02-24 15:37:12 +0100

    Hello Spryker

---
+++ src/Pyz/Yves/HelloSpryker/Controller/IndexController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Pyz\Yves\HelloSpryker\Controller;
+
+use Spryker\Yves\Kernel\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @method \Pyz\Client\HelloSpryker\HelloSprykerClientInterface getClient()
+ */
+class IndexController extends AbstractController
+{
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return array|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function indexAction(Request $request)
+    {
+        $data = ['helloWorld' => 'Hello Spryker!'];
+        return $this->view(
+            $data,
+            [],
+            '@HelloSpryker/views/index/index.twig'
+        );
+    }
+}
